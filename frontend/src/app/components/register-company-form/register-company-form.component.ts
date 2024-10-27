4fb01888c196db3db1ae257174631042581b5d1b
Yan Zampier <yanpzr1@gmail.com>
2024-10-27 18:33:00 -0300

    Alteração da página de criar empresa

--- frontend/src/app/components/register-company-form/register-company-form.component.ts
+++ frontend/src/app/components/register-company-form/register-company-form.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Component} from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
@@ -10,8 +10,8 @@ import {
   ValidationErrors,
   Validators,
 } from '@angular/forms';
-import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
-import { ToastrService } from 'ngx-toastr';
+import {NgxMaskDirective, NgxMaskPipe} from 'ngx-mask';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-register-company-form',
@@ -31,32 +31,54 @@ export class RegisterCompanyFormComponent {
 
   showNotificationAlert?: boolean;
 
+  postalCode: string = '';
+  street: string = '';
+  neighborhood: string = '';
+  city: string = '';
+  state: string = '';
+  region: string = '';
+
   constructor(private http: HttpClient, private fb: FormBuilder, private toastr: ToastrService) {
     this.registerCompanyForm = this.fb.group({
-      name: ['', Validators.required],
-      company: ['', Validators.required],
-      cnpj: ['', [Validators.required, this.validateCNPJ]],
-      phone: ['', [Validators.required, this.validatePhone]],
+      company: [{value: '', disabled: true}, Validators.required],
+      cnpj: [{value: '', disabled: true}, Validators.required, this.validateCNPJ],
+      phone: [{value: '', disabled: true}, Validators.required, this.validatePhone],
+      postalCode: [{value: '', disabled:true}, Validators.required],
+      street: [{value: '', disabled: true}], // Defina o estado inicial como desabilitado
+      neighborhood: [{value: '', disabled: true}],
+      city: [{value: '', disabled: true}],
+      state: [{value: '', disabled: true}],
     });
   }
 
   ngOnInit(): void {
-      // Carregar dados do usuário armazenados
-      const usuarioData = JSON.parse(localStorage.getItem('usuario') || '{}');
+    // Carregar dados do usuário armazenados
+    const cepData = JSON.parse(localStorage.getItem('cep') || '{}');
 
-      // Preenche o formulário de perfil com os dados do usuário
-      this.registerCompanyForm.patchValue({
-        name: usuarioData.nome,
-        company: '', // Adicione outros dados conforme necessário
-        cnpj: '' // Se houver CNPJ ou outros campos
-      });
+    // Preenche o formulário de perfil com os dados do usuário
+    this.registerCompanyForm.patchValue({
+      postalCode: cepData.postalCode
+    });
+
+    if (cepData.postalCode) {
+        cepData.postalCode
+        this.searchPostalCode();
+      }
+  }
+
+  // Método para alternar o estado de habilitado/desabilitado de um campo específico
+  toggleField(field: string) {
+    const control = this.registerCompanyForm.get(field);
+    if (control) {
+      control.enabled ? control.disable() : control.enable();
     }
+  }
 
   // Função de validação customizada para o CNPJ
   validateCNPJ(control: AbstractControl): ValidationErrors | null {
     const cnpj = control.value?.replace(/[^\d]+/g, ''); // Remove caracteres especiais
     if (cnpj?.length !== 14) {
-      return { invalidCNPJ: true };
+      return {invalidCNPJ: true};
     }
     return null;
   }
@@ -65,40 +87,50 @@ export class RegisterCompanyFormComponent {
   validatePhone(control: AbstractControl): ValidationErrors | null {
     const phone = control.value?.replace(/[^\d]+/g, ''); // Remove caracteres especiais
     if (phone?.length !== 10 && phone?.length !== 11) {
-      return { invalidPhone: true };
+      return {invalidPhone: true};
     }
     return null;
   }
 
-  // Submissão do formulário
-    onSubmit() {
-      if (this.registerCompanyForm.valid) {
-      const usuarioData = JSON.parse(localStorage.getItem('usuario') || '{}');
-      const userId = usuarioData.id_usuario;
-
-      const updatedData = {
-        nome: this.registerCompanyForm.value.name || usuarioData.nome,
-        email: usuarioData.email,
-        senha: usuarioData.senha,
-        id_usuario: userId
-      };
-
-        this.http.put(`http://localhost:8080/usuarios/${userId}`, updatedData).subscribe(
-            response => {
-              console.log('Dados atualizados com sucesso:', response);
-              this.toastr.success('Dados atualizados com sucesso!');
-
-              localStorage.setItem('usuario', JSON.stringify(updatedData));
-              },
-            error => {
-                console.error('Erro ao atualizar os dados:', error);
-                this.toastr.error('Erro ao atualizar os dados.');
+  // Função para buscar o CEP
+  searchPostalCode() {
+    const postalCodeValue = this.registerCompanyForm.get('postalCode')?.value;
+    if (postalCodeValue && postalCodeValue.length === 8) {
+      this.http
+        .get<any>(`https://viacep.com.br/ws/${postalCodeValue}/json/`)
+        .subscribe(
+          (data) => {
+            if (!data.erro) {
+              const dataForm = {
+                street: data.logradouro,
+                neighborhood: data.bairro,
+                city: data.localidade,
+                state: data.uf,
+                postalCode: data.cep.replace('-', ''),
               }
-          );
-        console.log('Formulário enviado:', this.registerCompanyForm.value);
-      } else {
-        console.log('Formulário inválido');
-        this.registerCompanyForm.markAllAsTouched();
-      }
+              this.registerCompanyForm.patchValue(dataForm);
+              localStorage.setItem('cep', JSON.stringify(dataForm));
+              this.registerCompanyForm.get('postalCode')?.disable();
+            } else {
+              this.toastr.error('CEP não encontrado.');
+            }
+          },
+          (error) => {
+            this.toastr.error('Erro ao buscar o CEP.');
+          }
+        );
+    } else {
+      this.toastr.warning('Por favor, insira um CEP válido com 8 dígitos.');
     }
+  }
+
+  // Submissão do formulário
+  onSubmit() {
+    if (this.registerCompanyForm.valid) {
+      console.log('Formulário enviado:', this.registerCompanyForm.value);
+    } else {
+      console.log('Formulário inválido');
+      this.registerCompanyForm.markAllAsTouched();
+    }
+  }
 }
