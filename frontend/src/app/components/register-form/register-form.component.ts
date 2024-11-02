729b68f6aa76a95bdd79fed9104f36565ace8b3c
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-11-02 10:38:52 -0300

    Refatoração: Alteração geral

--- frontend/src/app/components/register-form/register-form.component.ts
+++ frontend/src/app/components/register-form/register-form.component.ts
@@ -39,15 +39,26 @@ import {Usuario} from "../../models/user.model";
 })
 export class RegisterFormComponent {
   registerForm: FormGroup;
-  isLoading = false;
   errorMessage: any;
   showPassword = false; // Controle de visibilidade da senha
 
-  constructor(private fb: FormBuilder, private router: Router, private apiService: ApiUserService, private toastr: ToastrService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private apiService: ApiUserService,
+    private toastr: ToastrService
+  ) {
     this.registerForm = this.fb.group({
       nome: ['', [Validators.required]],
       email: ['', [Validators.required, this.validateEmail]],
-      senha: ['', [Validators.required, Validators.minLength(6)]],
+      senha: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(12), // Validação de no mínimo 12 caracteres
+          this.passwordValidator,
+        ],
+      ],
     });
   }
 
@@ -57,23 +68,42 @@ export class RegisterFormComponent {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (email && !emailPattern.test(email)) {
-      return {invalidEmail: true}; // Retorna um erro se o e-mail não for válido
+      return {invalidEmail: true};
     }
-    return null; // Retorna null se o e-mail for válido
+    return null;
   }
 
   togglePasswordVisibility() {
-    this.showPassword = !this.showPassword; // Alterna entre mostrar/ocultar senha
+    this.showPassword = !this.showPassword;
+  }
+
+  // Validação personalizada da senha
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.value;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumeric = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    const isValid =
+      password &&
+      password.length >= 12 && // Confere se a senha tem no mínimo 12 caracteres
+      hasUpperCase &&
+      hasLowerCase &&
+      hasNumeric &&
+      hasSpecialChar;
+
+    if (!isValid) {
+      return {invalidPassword: true};
+    }
+    return null;
   }
 
   register() {
     if (this.registerForm.valid) {
-      this.isLoading = true;
       const usuarioData: Usuario = this.registerForm.value;
 
       this.apiService.registerUser(usuarioData).subscribe({
         next: (response) => {
-          this.isLoading = false;
           this.toastr.success('Usuário registrado com sucesso!', '', {
             positionClass: 'toast-top-right',
             progressBar: true,
@@ -81,7 +111,10 @@ export class RegisterFormComponent {
             timeOut: 2000,
           });
 
-          const usuarioComId: Usuario = {...usuarioData, id_usuario: response.id_usuario};
+          const usuarioComId: Usuario = {
+            ...usuarioData,
+            id_usuario: response.id_usuario,
+          };
           localStorage.setItem('usuario', JSON.stringify(usuarioComId));
 
           setTimeout(() => {
@@ -89,10 +122,9 @@ export class RegisterFormComponent {
           }, 2500);
         },
         error: (error) => {
-          this.isLoading = false;
           this.errorMessage = error.message;
           this.toastr.error(error.message);
-        }
+        },
       });
     } else {
       this.registerForm.markAllAsTouched();
