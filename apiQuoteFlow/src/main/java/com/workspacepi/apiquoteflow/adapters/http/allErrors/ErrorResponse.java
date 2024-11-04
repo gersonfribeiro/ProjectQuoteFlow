bb722e8c7490331d70ec485b47468495da71afd0
gersin <gersonfernandesribeiro48@gmail.com>
2024-11-04 17:30:07 -0300

    Exceptions para autenticação negada

--- apiQuoteFlow/src/main/java/com/workspacepi/apiquoteflow/adapters/http/allErrors/ErrorResponse.java
+++ apiQuoteFlow/src/main/java/com/workspacepi/apiquoteflow/adapters/http/allErrors/ErrorResponse.java
@@ -3,6 +3,7 @@ package com.workspacepi.apiquoteflow.adapters.http.allErrors;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.workspacepi.apiquoteflow.domain.usuarios.Permissoes;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,9 @@ public class ErrorResponse {
     @JsonIgnore
     private String parametro;
 
+    @JsonProperty("permissao")
+    private Permissoes permissao;
+
 //  Propriedades genericas
     @JsonProperty("mensagem")
     private String mensagem;
@@ -35,17 +39,24 @@ public class ErrorResponse {
 
 //  Construtores para cada exception
 
-//  Exception de não encontrado por UUID
+//  Exception com UUID
     public ErrorResponse(String mensagem, UUID id, int status) {
         this.mensagem = mensagem;
         this.id = id;
         this.status = status;
     }
 
-//  Exception de não encontrado com string
+//  Exception com string
     public ErrorResponse(String mensagem, String parametro, int status) {
-        this.parametro = parametro;
         this.mensagem = mensagem;
+        this.parametro = parametro;
+        this.status = status;
+    }
+
+//  Exception com Permissao
+    public ErrorResponse(String mensagem, Permissoes permissao, int status) {
+        this.mensagem = mensagem;
+        this.permissao = permissao;
         this.status = status;
     }
 
