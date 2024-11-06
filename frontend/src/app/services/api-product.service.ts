2e72703e8d2807ae95ef2403053780cefcf924a3
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-11-06 11:04:07 -0300

    feat: Implementa método POST para salvar um produto

---
+++ frontend/src/app/services/api-product.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiProductService {
+
+  private apiUrlProduct = 'http://localhost:8080/id_empresa/produtos'
+
+  constructor(private http: HttpClient) {
+  }
+
+  // Método para salvar um produto
+  registerProduct(productData: any): Observable<any> {
+    return this.http.post(this.apiUrlProduct, productData)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Erro desconhecido';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Erro de rede: ${error.error.message}`;
+    } else {
+      switch (error.status) {
+        case 409:
+          errorMessage = 'Este produto já está registrado.';
+          break;
+        case 500:
+          errorMessage = 'Erro no servidor. Tente novamente mais tarde.';
+          break;
+        default:
+          errorMessage = 'Falha no registro. Tente novamente.';
+      }
+    }
+    return throwError(() => new Error(errorMessage));
+  }
+}
