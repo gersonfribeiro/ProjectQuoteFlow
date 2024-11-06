0a5e5fb39da47dc9372e72c86c036fe6b73b9473
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-11-06 09:42:00 -0300

    Refatoração: Resolvido problema na Página de Produtos

--- frontend/src/app/components/see-products-form/see-products-form.component.ts
+++ frontend/src/app/components/see-products-form/see-products-form.component.ts
@@ -2,19 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RegisterCompanyFormComponent} from "../register-company-form/register-company-form.component";
 import {SidebarDashboardComponent} from "../sidebar-dashboard/sidebar-dashboard.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
-  selector: 'app-see-products-page',
+  selector: 'app-see-products-form',
   standalone: true,
   imports: [
     RegisterCompanyFormComponent,
     SidebarDashboardComponent,
     CommonModule,
+    RouterLink,
   ],
   templateUrl: './see-products-form.component.html',
   styleUrls: ['./see-products-form.component.css']
 })
-export class SeeProductsPageComponent implements OnInit {
+export class SeeProductsFormComponent implements OnInit {
   products: any[] = []; // Declaração da variável products
 
   ngOnInit(): void {
