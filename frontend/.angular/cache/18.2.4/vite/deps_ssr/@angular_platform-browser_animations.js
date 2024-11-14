75e106cfc890ebcc5490645cd0f5fdd667e0c44e
gersin <gersonfernandesribeiro48@gmail.com>
2024-11-13 21:50:51 -0300

    a

---
+++ frontend/.angular/cache/18.2.4/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,27 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-WEDDZI4L.js";
+import "./chunk-WHQSNDI4.js";
+import "./chunk-ABO7Q5ZU.js";
+import "./chunk-L4UMD7IK.js";
+import "./chunk-EFQLMKET.js";
+import "./chunk-IAP3G6VX.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-PKCIKOYF.js";
+import "./chunk-VDZEJD3D.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
